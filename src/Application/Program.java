20bbbccb0856c341aa2dc6e9b0b1c8a0e3c84415
Luiz David Santin <luizdavidsantin.lds@gmail.com>
2023-08-19 14:56:14 -0300

    class position

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,11 +1,15 @@
 package Application;
 
+import Boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		System.out.println("ola mundo");
+		
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 	}
 
 }
