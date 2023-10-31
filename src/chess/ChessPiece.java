5d0a01347c9323b238a71c797e175ae8aa59f858
Luiz <luiz.santin@swa.com.br>
2023-10-31 16:44:03 -0300

    check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,19 +4,21 @@ import Boardgame.Board;
 import Boardgame.Piece;
 import Boardgame.Position;
 
-public abstract class ChessPiece  extends Piece{
-	
+public abstract class ChessPiece extends Piece {
+
 	private Color color;
 
-	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
 
-
 	public Color getColor() {
 		return color;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
